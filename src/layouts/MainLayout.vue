01b7affd6802bd0331bdddc8e7549441addd65aa
Kinda Hall <1142704468@qq.com>
2023-07-23 22:17:41 +0800

    feat: 🎸 修改上传组件

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,7 +2,7 @@
   <q-layout view="hHh Lpr lFf">
     <q-header elevated>
       <q-toolbar>
-        <q-toolbar-title>
+        <q-toolbar-title class="cursor-pointer" @click="goHomePage">
           {{ title }}
         </q-toolbar-title>
         <div>{{ version }}</div>
@@ -17,9 +17,16 @@
 
 <script setup lang="ts">
 import { ref } from 'vue';
+import { useRouter } from 'vue-router';
 import { storeToRefs } from 'pinia';
 import { useAppStore } from 'src/stores/app';
 
 const appStore = useAppStore();
 const { version, title } = storeToRefs(appStore);
+
+const router = useRouter()
+
+const goHomePage = () => {
+  router.push('/')
+}
 </script>
--- src/pages/ImageUtils.vue
+++ src/pages/ImageUtils.vue
@@ -1,41 +1,113 @@
 <template>
-  <q-page class="q-pa-md">
-    {{ tab }}
-    <q-file v-model="image" label="选择图片" filled counter :counter-label="counterLabelFn" max-files="1"
-      style="max-width: 300px">
-      <template v-slot:prepend>
-        <q-icon name="attach_file" />
+  <q-page :style-fn="myTweak">
+    <q-splitter v-model="splitterModel" class="full-width" style="height: 100%;">
+      <template v-slot:before>
+        <div class="q-pa-md">
+          <div class="full-width image-upload-box flex justify-center items-center rounded-borders column cursor-pointer"
+            :style="{ backgroundColor: uploadStatus ? '#f2f2f2' : '', backgroundImage: `url(${origin_image_url})` }"
+            @click="onUploadFile">
+            <div class="column justify-center items-center q-gutter-xs" v-if="!uploadStatus">
+              <q-icon name="cloud_upload" style="font-size: 2em" class="text-primary" />
+              <div>
+                点击上传图片
+              </div>
+            </div>
+            <div class="column justify-center items-center q-gutter-xs" v-if="uploadStatus">
+              <q-icon name="refresh" style="font-size: 2em" class="text-primary refresh" />
+              <div>上传中...</div>
+            </div>
+            <input ref="inputRef" multiple="false" accept="image/*" name="image" id="image" type="file" v-show="false"
+              @change="handleChange" />
+          </div>
+          <q-btn color="primary" label="压缩图片" />
+        </div>
       </template>
-    </q-file>
-    <q-btn color="primary" label="压缩图片" @click="uploadImage" />
-    <div>原图：</div>
-    <div>压缩后：</div>
+
+      <template v-slot:after>
+        <div class="q-pa-md">
+          <div>压缩后：</div>
+          <q-btn color="primary" label="下载" @click="downLoadZipedImage" />
+        </div>
+      </template>
+
+    </q-splitter>
   </q-page>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
-import { useRoute } from 'vue-router';
+import { nextTick, ref } from 'vue';
 import { uploadFile } from '@/services/file';
 import { getOssClient } from '@/services/file';
 
-const route = useRoute()
-const { operate } = route.query;
-const tab = ref(operate)
-const image = ref<File | null>(null)
+const inputRef = ref(null);
+const uploadStatus = ref(false);
+const zipStatus = ref(false);
+const origin_image = ref<File | null>(null);
+const ziped_image = ref<string | null>(null);
+const origin_image_url = ref<string | null>(null);
+const splitterModel = ref(50); // start at 50%
 
-const counterLabelFn = ({ totalSize, filesNumber, maxFiles }: any) => {
-  return `${filesNumber} files of ${maxFiles} | ${totalSize}`
+const uploadImage = async (file: File) => {
+  if (file) {
+    const ossClient = await getOssClient('kinda-tools');
+    const attachment = await uploadFile(ossClient, '/image', file);
+    const url = ossClient && ossClient.signatureUrl(attachment.name, { expires: 600 });
+    return url;
+  }
+  return null;
 }
 
-const uploadImage = async () => {
-  if (image.value) {
-    const ossClient = await getOssClient('kinda-tools');
-    const attachment = await uploadFile(ossClient, '/image', image.value);
-    console.log(111, attachment)
-    const url = ossClient && ossClient.signatureUrl(attachment.name, { expires: 600 });
-    console.log(222, url)
-  }
+const onUploadFile = () => {
+  if (uploadStatus.value) return;
+  inputRef.value && inputRef.value.click();
+}
+
+const handleChange = async (e: Event) => {
+  const files = (e.target as HTMLInputElement).files
+  if (!files) return
+  uploadStatus.value = true;
+  origin_image.value = files[0];
+  const data = await uploadImage(files[0]);
+  origin_image_url.value = data
+  uploadStatus.value = false;
+}
+
+const downLoadZipedImage = () => {
+  console.log(111)
+}
+
+const myTweak = (offset: number) => {
+  return { height: offset ? `calc(100vh - ${offset}px)` : '100vh' }
 }
 
 </script>
+<style lang="scss">
+.image-upload-box {
+  min-height: 200px;
+  border: 1px dotted #aaa;
+  transition: all .1s ease-in-out;
+  background-size: cover;
+  background-attachment: fixed;
+  background-repeat: no-repeat;
+}
+
+.image-upload-box:hover {
+  background-color: #f2f2f2;
+}
+
+.refresh {
+  -webkit-animation: rotate-center 1s ease-in-out infinite both;
+  animation: rotate-center 1s ease-in-out infinite both;
+}
+
+@keyframes rotate-center {
+  0% {
+    transform: rotate(0);
+  }
+
+  100% {
+    transform: rotate(360deg);
+  }
+
+}
+</style>
