b272b09e40073695fe851e80715cf8402462b577
Kinda Hall <1142704468@qq.com>
2023-07-24 21:46:54 +0800

    feat: 🎸 美化样式

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,11 +1,11 @@
 <template>
-  <q-page class="q-pa-md" :style-fn="myTweak">
+  <q-page class="q-pa-md index" :style-fn="myTweak">
     <q-banner rounded class="bg-primary text-white q-mb-md">
-      我会慢慢的开发一些比较实用的小功能，方便以后使用，统一整理在这里。暂时建议使用电脑端Safrai/Chrome浏览器。
+      欢迎使用Kinda的工具箱，我会慢慢的开发一些比较实用的小功能，方便以后使用，统一整理在这里。暂时建议使用电脑端Safrai/Chrome浏览器。
     </q-banner>
     <KindaTools></KindaTools>
   </q-page>
-  <div class="flex justify-center items-center column q-pa-md bg-primary">
+  <div class="flex justify-center items-center column q-pa-md bg-primary text-white">
     <div>
       Copyright © 2023 Kinda
     </div>
@@ -22,5 +22,41 @@ import { ref } from 'vue';
 const myTweak = (offset: number) => {
   return { height: offset ? `calc(100vh - ${offset}px)` : '100vh' }
 }
-
 </script>
+<style lang="scss">
+.body--dark {
+  .index::after {
+    background-color: #00000044;
+  }
+}
+
+.body--light {
+  .index::after {
+    background-color: #00000022;
+  }
+}
+
+.index::after {
+  content: '';
+  z-index: -1;
+  position: absolute;
+  left: 0;
+  top: 0;
+  backdrop-filter: blur(20px);
+  width: 100%;
+  height: 100%;
+}
+
+.index::before {
+  content: '';
+  z-index: -1;
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  background-attachment: fixed;
+  background-size: cover;
+  background-image: url('https://oss.kinda.info/image/202307242146672.jpg') !important;
+}
+</style>
